32bdf529e969f37cf617a5779fd2adf3d098b085
OleksiiKhomenko <homenko0240@gmail.com>
2023-05-07 04:25:13 +0300

    add opportunity to view drivers;

--- website/src/auth.py
+++ website/src/auth.py
@@ -1,92 +1,10 @@
-import flask_login
-import psycopg2.errors
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from src import cursor
+from src.models import UserDatabase
 
 auth = Blueprint("auth", __name__)
-
-
-class User(UserMixin):
-    # __slots__ = ["user_id", "user_email", "user_password", "role_id", "name", "surname", "surname2",
-    #              "is_active", "is_authenticated"]
-
-    def __init__(self, user_id: int = -1, role_id: int = 4, user_email: str = "", user_password: str = "",
-                 name: str = "", surname: str = "", surname2: str = ""):
-        """
-
-        :param user_id:
-        :param role_id: 1-'owner', 2-'director', 3-'driver', 4-'no-role'
-        :param user_email:
-        :param user_password:
-        :param name:
-        :param surname:
-        :param surname2:
-        """
-        self.id = user_id
-        self.role_id = role_id
-        self.email = user_email
-        self.password = user_password
-        self.name = name
-        self.surname = surname
-        self.surname2 = surname2
-
-
-class UserDatabase:
-    db_cursor = cursor
-
-    @staticmethod
-    def add_user(user_email, user_password, role_id=4, name="", surname="", surname2=""):
-        sql_query = f"""
-                    insert into trolleybus_site_database.users (user_id, user_email, user_password, 
-                    role_id, name, surname, surname2)
-                    values (nextval('trolleybus_site_database.users_seq'), '{user_email}', 
-                    '{user_password}', {role_id}, 
-                    '{name}', '{surname}', '{surname2}');
-                    """
-
-        try:
-            UserDatabase.db_cursor.execute(sql_query)
-            UserDatabase.db_cursor.execute("commit;")
-        except Exception as e:
-            UserDatabase.db_cursor.execute("commit;")
-            raise e
-
-    @staticmethod
-    def get_user_by_email(user_email) -> User:
-        sql_query = f"""
-                    select *
-                    from trolleybus_site_database.users
-                    where trolleybus_site_database.users.user_email = '{user_email}';
-                    """
-
-        UserDatabase.db_cursor.execute(sql_query)
-        user_info = cursor.fetchone()
-
-        if user_info is not None:
-            user = User(*user_info)
-        else:
-            user = None
-        return user
-
-    @staticmethod
-    def get_user_by_id(user_id) -> User:
-        sql_query = f"""
-                    select *
-                    from trolleybus_site_database.users
-                    where trolleybus_site_database.users.user_id = {user_id};
-                    """
-
-        UserDatabase.db_cursor.execute(sql_query)
-        user_info = cursor.fetchone()
-
-        if user_info is not None:
-            user = User(*user_info)
-        else:
-            user = None
-        return user
 
 
 @auth.route("/register.html", methods=["GET", "POST"])
@@ -130,12 +48,17 @@ def login():
         # incorrect email
         if user is None:
             flash("Користувача з вказаною поштою не існує.", category="error")
-            return render_template("login.html", boolean=True)
+            return render_template("login.html", user=current_user)
+
+        # for development purposes
+        elif ":" not in user.password and user.password == user_password:
+            login_user(user, remember=True)
+            return redirect(url_for("views.main"))
 
         # incorrect password
         elif not check_password_hash(user.password, user_password):
             flash("Не правильний пароль.", category="error")
-            return render_template("login.html", boolean=True)
+            return render_template("login.html", user=current_user)
 
         else:
             login_user(user, remember=True)
